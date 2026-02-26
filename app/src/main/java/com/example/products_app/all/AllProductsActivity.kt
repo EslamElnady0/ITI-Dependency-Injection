2a355f879e041c34d8ae48aadc5929b25fd5ce45
EslamElnady0 <eslamelnady254@gmail.com>
2026-02-26 21:24:17 +0200

    koin impl done

--- app/src/main/java/com/example/products_app/all/AllProductsActivity.kt
+++ app/src/main/java/com/example/products_app/all/AllProductsActivity.kt
@@ -6,13 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.lifecycle.ViewModelProvider
 import com.example.di_starterapplication.data.repository.ProductsRepository
 import com.example.products_app.fav.FavProductsViewModel
+import org.koin.androidx.compose.koinViewModel
 import org.koin.compose.koinInject
 
 class AllProductsActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val viewModel = koinInject<AllProductsViewModel>()
+            val viewModel = koinViewModel<AllProductsViewModel>()
             AllProductsScreen(viewModel)
         }
     }
--- app/src/main/java/com/example/products_app/fav/FavProductsActivity.kt
+++ app/src/main/java/com/example/products_app/fav/FavProductsActivity.kt
@@ -3,13 +3,14 @@ package com.example.products_app.fav
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import org.koin.androidx.compose.koinViewModel
 import org.koin.compose.koinInject
 
 class FavProductsActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val viewModel = koinInject<FavProductsViewModel>()
+            val viewModel = koinViewModel<FavProductsViewModel>()
             FavProductsScreen(viewModel)
         }
     }
